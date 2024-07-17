dcf9416c5477a3055cc7186792a834f5a3dabd4d
Yoomin Kim <yoomining02@snu.ac.kr>
2024-07-17 11:39:55 +0900

    rough work

--- rough_work.py
+++ rough_work.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6782dd1ffcc1dc2054986210ffb21e6633deb4365d2a7296c241b45367fbb0a
-size 2212
+oid sha256:afad058ce4b5572aa30ad9cb551560e5b25980ea970e4af1da2994850e3827de
+size 1972
