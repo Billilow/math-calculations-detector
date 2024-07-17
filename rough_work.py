e64d428aba79bdbdeab715f5a98c53d32f8f562a
Elena <elena.guo1405@gmail.com>
2024-07-17 18:46:41 +0900

    Included calculation result and frame but needs improvent and an extra window for the result

--- rough_work.py
+++ rough_work.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a996ed0980a681a3e36af4c951fefa8792100bc2414067605002947937069f2d
-size 2555
+oid sha256:b8c7a07f5eef1fe7ccf9e02adc0f8f99eb47594f22bd9fa0357a9e1e64cf9e4a
+size 3280
