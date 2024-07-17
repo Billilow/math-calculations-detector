29160eb0660f5faf8d48bc7e1a0a53d74ab2f610
Elena <elena.guo1405@gmail.com>
2024-07-17 11:07:59 +0900

    Rough version edit

---
+++ Rough work.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbed1db1e226c941809966e5013d2e46e1895a05ed18139c3321e4e0837f94f1
+size 518
---
+++ maths.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd54fd96275875ec797882ccc9fa5f5094d7c14e1965d5af44d810f6fb0f8f87
+size 493
