d109a3c6a57b71c94d23729c110deca71bfb1290
Elena Guo <elena.guo1405@gmail.com>
2024-07-17 11:51:36 +0900

    Update rough_work.py

--- rough_work.py
+++ rough_work.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6782dd1ffcc1dc2054986210ffb21e6633deb4365d2a7296c241b45367fbb0a
-size 2212
+oid sha256:4d0254f02ac32aa84503acb344d699c9c712a0ca67b2b8cacad234db0eee3805
+size 2206
