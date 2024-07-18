b841b5da0383edf5aa449971bde9b7757c634eef
Yoomin Kim <yoomining02@snu.ac.kr>
2024-07-18 10:03:30 +0900

    I've updated the result frame

--- rough_work.py
+++ rough_work.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8c7a07f5eef1fe7ccf9e02adc0f8f99eb47594f22bd9fa0357a9e1e64cf9e4a
-size 3280
+oid sha256:a33ccb31ef8446f372075a6884b8a436ddc4ba88c3eaea5c3e3ca848d0bea719
+size 3410
