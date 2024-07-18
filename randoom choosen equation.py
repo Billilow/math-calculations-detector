7edcb21841b0e753524037427e935a604359e1bb
Nina <ncaradjova@gmail.com>
2024-07-18 18:16:30 +0900

    added some files
    Former-commit-id: 4a8c68c66fac025ff4e4a54bdde52d281aff196b

--- randoom choosen equation.py
+++ randoom choosen equation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aab4a4bd16a5f6fa8d9afed84c69abd6dd9e3923ea857deab043cb4cfc8b7a08
-size 6021
+oid sha256:9a9a63458c9bde108596d2abca221bc04b11d8dd6c23b4ac81283ff4934ab12f
+size 5333
