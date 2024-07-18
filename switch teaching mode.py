fb19f77abafe16862dca2cfd7aae198b761a48fd
Yoomin Kim <yoomining02@snu.ac.kr>
2024-07-18 11:03:45 +0900

    some changes to files
    Former-commit-id: 98164b804977c3151b08526da27e54ddb1cfa3d3

--- switch teaching mode.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:58b49d29795f205f37fdc9debe961cbcbd517bf4b325cbe95e974e9738cfe13b
-size 3992
---
+++ switch_teaching _mode.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:91aa82109979491417ca480149d74c235410d32ab33f5ceef0fd17fdde755fe5
+size 3989
