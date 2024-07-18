3b69c9736d490a12564a86650b4feb452d339fc7
Nina <ncaradjova@gmail.com>
2024-07-18 11:12:53 +0900

    added some files
    Former-commit-id: 77fe38c50ccbb9c1ea780f77e14c7d51f7e6b2c0

--- switch_teaching _mode.py
+++ switch_teaching _mode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58b49d29795f205f37fdc9debe961cbcbd517bf4b325cbe95e974e9738cfe13b
-size 3992
+oid sha256:33409ad619529d74726d40c9d7701c3a00768444c35cf0bcb6a6c033f6c13218
+size 5493
