d5bcd3216d92118ba3735ba307f6fb7ff559fad8
Nina <ncaradjova@gmail.com>
2024-07-19 20:07:02 +0900

    added some files
    Former-commit-id: 0d37574b0d6644b50c89d65c6f5cb3bf0731b237

--- new one.py
+++ new one.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20bcf55b88fe529ceb352c35133f59d97784852e3edef1efb03ba8a9f5940baa
-size 3862
+oid sha256:26c0ada1689eca7f975dd4e88e08c05e83b203faba91bb2dcf3025004c451237
+size 5690
