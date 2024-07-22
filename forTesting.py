140b4ece5fc0fcc84ee4085fdfb3993f0282fe84
Yoomin Kim <yoomining02@snu.ac.kr>
2024-07-22 09:46:42 +0900

    forTesting file edited but the robot is not moving
    Former-commit-id: cb150336a88b52308553f6793b06ffbbdcfdebff

--- forTesting.py
+++ forTesting.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8915db468f6107ebd23b5d2d1cbd5559e5afa9a53c2e287d6d3386dea4077f63
-size 10625
+oid sha256:f4a865cea64e16234f356edd348a947b16c2e997a69bc0eed22ff28a88697123
+size 11153
